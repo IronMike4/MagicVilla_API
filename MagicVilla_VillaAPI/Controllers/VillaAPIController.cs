4d2f99f99d51cf91f7e2ca9feea56edd9c3e64f1
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-10-19 20:05:00 +0200

    Added Put endpoint

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -31,6 +31,7 @@ public class VillaAPIController : ControllerBase
     {
       return NotFound();
     }
+
     return Ok(villa);
   }
 
@@ -63,6 +64,7 @@ public class VillaAPIController : ControllerBase
       return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
     }
   }
+
   [HttpDelete("{id:int}", Name = "DeleteVilla")]
   [ProducesResponseType(StatusCodes.Status204NoContent)]
   [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -75,6 +77,7 @@ public class VillaAPIController : ControllerBase
       return BadRequest();
 
     }
+
     var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
     if (villa == null)
     {
@@ -84,4 +87,22 @@ public class VillaAPIController : ControllerBase
     VillaStore.villaList.Remove(villa);
     return NoContent();
   }
+
+  [HttpPut("{id:int}", Name = "DeleteVilla")]
+  [ProducesResponseType(StatusCodes.Status204NoContent)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+  {
+    if (villaDTO == null || id != villaDTO.Id)
+    {
+      return BadRequest();
+    }
+    var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+    villa.Name = villaDTO.Name;
+    villa.Sqft = villaDTO.Sqft;
+    villa.Occupancy = villaDTO.Occupancy;
+
+    return NoContent();
+  }
+
 }
