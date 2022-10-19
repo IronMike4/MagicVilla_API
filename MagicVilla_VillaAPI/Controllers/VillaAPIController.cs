6fb15d33a309d42b66d65e477d8b5ce6e2b36524
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-10-19 19:22:17 +0200

    Added model validations for required fields

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -35,12 +35,17 @@ public class VillaAPIController : ControllerBase
   }
 
   [HttpPost]
-  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status201Created)]
   [ProducesResponseType(StatusCodes.Status400BadRequest)]
   [ProducesResponseType(StatusCodes.Status500InternalServerError)]
   public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
   {
     {
+      //if (!ModelState.IsValid)
+      //{
+      //  return BadRequest(ModelState);
+      //}
+
       if (villaDTO == null)
       {
         return BadRequest(villaDTO);
